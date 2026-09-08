fab3d61bae3f16a681cc009bddbf89fa01d43352
Sai Krishna Goud <73684151+saikrishnask15@users.noreply.github.com>
2026-09-08 17:15:53 +0530

    Fix: Join class declaration on single line - resolves BeanDefinitionStoreException

--- src/main/java/com/example/LibraryManagementSystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/LibraryManagementSystem/LibraryManagementSystemApplication.java
@@ -6,12 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class
-LibraryManagementSystemApplication {
+public class LibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
 
 }
-
